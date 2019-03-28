6f98e2585add782123c66dc1c3932af0615fba6c
Sam Sendelbach <sbsends@gmail.com>
2019-03-28 17:44:06 -0500

    index.js: removed grpc cipher suite

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const protoLoader = require('@grpc/proto-loader');
-const grpc = require('@grpc/grpc-js');
-// const grpc = require('grpc');
+// const grpc = require('@grpc/grpc-js');
+const grpc = require('grpc');
 // request works when using grpc
 // but not when using @grpc/grpc-js
 
@@ -18,7 +18,7 @@ const grpcOptions = {
 
 const packageDefinition = protoLoader.loadSync("rpc.proto", grpcOptions);
 const packageObject = grpc.loadPackageDefinition(packageDefinition);
-process.env.GRPC_SSL_CIPHER_SUITES = 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384'
+// process.env.GRPC_SSL_CIPHER_SUITES = 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384'
 
 // var mac = fs.readFileSync("admin.macaroon");
 // const metadata = new grpc.Metadata()
